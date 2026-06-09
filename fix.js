1f40c72a61cdcec4f73abed6eac5e521a98591f9
Aitna <139067192+holalala1010@users.noreply.github.com>
2026-06-09 19:51:26 +0200

    Delete fix.js

--- fix.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('./src/App.tsx', 'utf8');
-code = code.replace(/document\.createElement\('a-sphere'\)/g, "document.createElement('a-octahedron')");
-code = code.replace(/<a-cone /g, '<a-cone segments-radial="4" ');
-code = code.replace(/document\.createElement\('a-cylinder'\)/g, "document.createElement('a-box')");
-code = code.replace(/particle\.setAttribute\('radius', radius\.toString\(\)\);/g, "particle.setAttribute('width', (radius*2).toString()); particle.setAttribute('depth', (radius*2).toString());");
-fs.writeFileSync('./src/App.tsx', code);
